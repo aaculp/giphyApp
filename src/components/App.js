085148b7c2d7ab2e18309e0172fbcaf804d23706
Aaron Culp <aaron_culp@anfcorp.com>
2020-02-24 15:58:38 -0500

    added the async await verion of the function onSearchSubmit

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,27 @@ import axios from 'axios';
 import Searchbar from './Searchbar';
 
 export default class App extends Component {
-    onSearchSubmit = (term) => {
-        axios.get('https://api.unsplash.com/search/photos?', {
+    // onSearchSubmit = (term) => {
+    //     axios.get('https://api.unsplash.com/search/photos?', {
+    //         params: { query: term },
+    //         headers: {
+    //             Authorization: 'Client-ID KK_TY13-B2AIZ6_ywa-6cg8TmteMfKET2qxTO5VC2Fc'
+    //         }
+    //     }).then((response) => {
+    //         console.log(response.data.results)
+    //     })
+    // }
+
+    // this is async and await
+    async onSearchSubmit(term) {
+        const response = await axios
+        .get('https://api.unsplash.com/search/photos?', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID KK_TY13-B2AIZ6_ywa-6cg8TmteMfKET2qxTO5VC2Fc'
             }
-        }).then((response) => {
-            console.log(response)
         })
+        console.log(response.data.results)
     }
 
     render() {
