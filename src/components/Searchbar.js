5a24d8a8b14ee1c9e7e0c8277ded4bf7ffa50bc5
Aaron Culp <aaron_culp@anfcorp.com>
2020-02-24 13:36:38 -0500

    prior update

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -9,14 +9,20 @@ export default class Searchbar extends Component {
     //     this.setState({input: e.target.value})
     //     console.log(this.state.input)
     // }
+    
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state.input)
+    }
 
     render() {
         return (
             <div className='ui segment'>
-                <form className= 'ui form'>
-                    <div className ='field'>
+                <form className='ui form' onSubmit={this.onFormSubmit}>
+                    <div className='field'>
                         <label>Image Search</label>
-                        <input type='text' value = {this.state.input} onChange ={(e) => this.setState({input: e.target.value})}/>
+                        <input type='text' value={this.state.input} onChange={(e) => this.setState({input: e.target.value})}/>
+                        {/* <input type='text' value={this.state.input} onChange={(e) => this.setState({input: e.target.value.toUpperCase()})}/> */}
                         {/* <input type='text' onChange={(event) => console.log(event.target.value)}/> */}
                     </div>
                 </form>
