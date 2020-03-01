56ed0dbfb0b0e0b5e0b20b471d5dc9712a3f3ddb
aaculp <aaculp@icloud.com>
2020-03-01 15:05:51 -0500

    added refs to Image Card component...as well as adding the component if I didnt already commit

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
 
 export default class ImageCard extends Component {
+    state = {
+        imgRef: React.createRef()
+    }
+
+    componentDidMount() {
+        console.log(this.state.imgRef.current.clientHeight)
+    }
+
     render() {
         const { id, description, urls } = this.props.image;
         return (
             <div>
                 <img
+                    ref={this.state.imgRef}
                     id={id}
                     alt={description}
                     src={urls.regular}
