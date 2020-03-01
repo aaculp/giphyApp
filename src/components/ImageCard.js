76552d2a65ae12c45966029690c941ba83e1e01b
aaculp <aaculp@icloud.com>
2020-03-01 15:29:58 -0500

    added setSpans to really get a handle of refs. added inline style to make up css

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react'
 
 export default class ImageCard extends Component {
     state = {
-        imgRef: React.createRef()
+        imgRef: React.createRef(),
+        spans: 0,
     }
 
     componentDidMount() {
-        console.log(this.state.imgRef.current.clientHeight)
+        // console.log(this.state.imgRef.current.clientHeight)
+        this.state.imgRef.current.addEventListener('load', this.setSpans);
+    }
+
+    // calling set spans because CSS grid-row is spans
+    setSpans = () => {
+        const height = this.state.imgRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+
+        this.setState({ spans })
     }
 
     render() {
         const { id, description, urls } = this.props.image;
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img
                     ref={this.state.imgRef}
                     id={id}
